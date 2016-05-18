d781435c25d50d9329b4319cc9f2d00038c1dd73
Garry Marsland <garry@basefour.co.uk>
2016-05-18 12:42:21 +0100

    Split scraping and reading functionality
    Update scraper to write headers

--- src/BuiltInLondon.Scraper/Program.cs
+++ src/BuiltInLondon.Scraper/Program.cs
@@ -12,12 +12,31 @@ namespace BuiltInLondon.Scraper
 {
     class Program
     {
-        private static readonly Uri baseAddress = new Uri("http://www.builtinlondon.com/");
+        private static readonly Uri BaseAddress = new Uri("http://www.builtinlondon.com/");
+        private static readonly string FilePath = ConfigurationManager.AppSettings["FilePath"];
 
         static void Main()
         {
-            var filePath = ConfigurationManager.AppSettings["FilePath"];
+            Scrape();
+            //Read();
+        }
 
+        private static void Read()
+        {
+            using (var streamReader = new StreamReader(FilePath))
+            using (var csvReader = new CsvReader(streamReader))
+            {
+                csvReader.Configuration.RegisterClassMap<CompanyClassMap>();
+
+                foreach (var company in csvReader.GetRecords<Company>())
+                {
+                    Console.WriteLine(company.Name);
+                }
+            }
+        }
+
+        private static void Scrape()
+        {
             var html = Get("/");
 
             var document = new HtmlDocument();
@@ -26,13 +45,13 @@ namespace BuiltInLondon.Scraper
             var ul = document.DocumentNode.SelectNodes("//ul").SingleOrDefault(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("startup-list"));
             Contract.Assert(ul != null);
 
-            //var companies = new List<Company>();
             var companyParser = new CompanyParser();
 
-            using (var streamWriter = new StreamWriter(filePath))
+            using (var streamWriter = new StreamWriter(FilePath))
             using (var csvWriter = new CsvWriter(streamWriter))
             {
                 csvWriter.Configuration.RegisterClassMap<CompanyClassMap>();
+                csvWriter.WriteHeader<Company>();
 
                 foreach (var li in ul.SelectNodes("li"))
                 {
@@ -40,21 +59,19 @@ namespace BuiltInLondon.Scraper
                     Console.WriteLine(url);
                     var companyHtml = Get(url);
                     var company = companyParser.Parse(url, companyHtml);
-                    //companies.Add(company);
 
                     csvWriter.WriteRecord(company);
                     streamWriter.Flush();
                     Thread.Sleep(3000);
                 }
-
             }
-       }
+        }
 
         private static string Get(string relativeUrl)
         {
             using (var client = new HttpClient())
             {
-                client.BaseAddress = baseAddress;
+                client.BaseAddress = BaseAddress;
                 return client.GetStringAsync(relativeUrl).Result;
             }
         }
